c12f863be901e35e0d979eb077345bc78b58c4bc
cowcow1 <stevanusfirman22@gmail.com>
2023-10-15 14:06:57 +0700

    ADDING_OOP
    FIFTH_COMMIT

--- UTSLEC/index.php
+++ UTSLEC/index.php
@@ -1,3 +1,19 @@
+<?php
+session_start();
+
+$dsn = "mysql:host=localhost;dbname=utslecpemweb";
+$kunci = new PDO($dsn, "root", "");
+
+$jenismenu = [
+    "appetizer" => "Appetizer",
+    "main course" => "Main Course",
+    "dessert" => "Dessert",
+    "seafood" => "Sea Food",
+    "vegetables" => "Vegetables",
+    "drink" => "Drink"
+];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,10 +25,23 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
 </head>
 
+<style>
+    .menu-details {
+        max-height: 0;
+        overflow: hidden;
+        transition: max-height 0.3s ease;
+    }
+
+    .menu-details-active {
+        max-height: 1000px;
+        transition: max-height 2s ease;
+    }
+</style>
+
 <body>
     <div class="dark:bg-gray-900">
-        <header class="absolute inset-x-0 top-0 z-50" style="position:fixed; background-color:white">
-            <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+        <header class="absolute inset-x-0 top-0 z-50">
+            <nav class="bg-white dark:bg-gray-900 w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
                 <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
                     <a href="index.php" class="flex items-center">
                         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Afungnima</span>
@@ -76,7 +105,7 @@
                                 <img src="https://st4.depositphotos.com/3860131/24657/i/450/depositphotos_246574494-stock-photo-deviled-eggs-stuffed-eggs-filled.jpg" alt="Makanan Appetizer" class="h-full w-full object-cover object-center">
                             </div>
                             <h3 class="mt-3 text-sm text-white">
-                                <a href="#">
+                                <a href="#sectionappetizer">
                                     <span class="absolute inset-0"></span>
                                     Appetizer
                                 </a>
@@ -88,7 +117,7 @@
                                 <img src="https://st2.depositphotos.com/3210553/9823/i/450/depositphotos_98232150-stock-photo-pan-fried-salmon-with-tender.jpg" alt="Makanan Utama" class="h-full w-full object-cover object-center">
                             </div>
                             <h3 class="mt-4 text-sm text-white">
-                                <a href="#">
+                                <a href="#sectionmain course">
                                     <span class="absolute inset-0"></span>
                                     Main Course
                                 </a>
@@ -100,7 +129,7 @@
                                 <img src="https://st.depositphotos.com/1744806/2387/i/450/depositphotos_23875029-stock-photo-dessert-cheesecake.jpg" alt="Makanan Penutup" class="h-full w-full object-cover object-center">
                             </div>
                             <h3 class="mt-4 text-sm text-white">
-                                <a href="#">
+                                <a href="#sectiondessert">
                                     <span class="absolute inset-0"></span>
                                     Dessert
                                 </a>
@@ -112,7 +141,7 @@
                                 <img src="https://st.depositphotos.com/1158226/2406/i/450/depositphotos_24068577-stock-photo-shrimps-fried-on-pan-with.jpg" alt="Seafood" class="h-full w-full object-cover object-center">
                             </div>
                             <h3 class="mt-4 text-sm text-white">
-                                <a href="#">
+                                <a href="#sectionseafood">
                                     <span class="absolute inset-0"></span>
                                     Sea Food
                                 </a>
@@ -124,7 +153,7 @@
                                 <img src="https://st.depositphotos.com/1018248/2279/i/450/depositphotos_22790206-stock-photo-vegetarian-food-on-white-dish.jpg" alt="Sayuran" class="h-full w-full object-cover object-center">
                             </div>
                             <h3 class="mt-4 text-sm text-white">
-                                <a href="#">
+                                <a href="#sectionvegetables">
                                     <span class="absolute inset-0"></span>
                                     Vegetables
                                 </a>
@@ -136,7 +165,7 @@
                                 <img src="https://st2.depositphotos.com/1761693/11003/i/450/depositphotos_110035832-stock-photo-assortment-of-iced-fruit-drinks.jpg" alt="Minuman" class="h-full w-full object-cover object-center">
                             </div>
                             <h3 class="mt-4 text-sm text-white">
-                                <a href="#">
+                                <a href="#sectiondrink">
                                     <span class="absolute inset-0"></span>
                                     Drink
                                 </a>
@@ -147,6 +176,48 @@
                 </div>
             </div>
         </section>
+        <?php foreach ($jenismenu as $key => $value) : ?>
+            <section class="dark:bg-gray-900" id="section<?= $key ?>">
+                <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                    <h2 class="text-2xl font-bold text-white"><?= $value ?></h2>
+                    <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+                        <?php
+                        $sql = "SELECT * FROM daftarmenu WHERE jenismenu = ?";
+                        $stmt = $kunci->prepare($sql);
+                        $stmt->execute([$key]);
+
+                        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
+                        ?>
+                            <div style="display: flex; flex-direction:column">
+                                <div class="w-full max-w-md bg-gray-800 rounded-lg shadow-md px-4 py-4">
+                                    <div class="group relative" data-aos="zoom-in" onclick="toggleDetails(<?= $row['idmenu'] ?>)">
+                                        <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                                            <?php echo "<img src='fotomenu/" . $row["fotomenu"] . "' class='h-full w-full object-cover object-center'>"; ?>
+                                        </div>
+                                        <h3 class="mt-3 text-sm text-white">
+                                            <a>
+                                                <span class="absolute inset-0"></span>
+                                                <?= $row['namamenu'] ?>
+                                            </a>
+                                        </h3>
+                                    </div>
+                                    <div id="details_<?= $row['idmenu'] ?>" class="menu-details">
+                                        <p class="text-base font-semibold text-white my-1"><?= $row['deskripsimenu'] ?></p>
+                                        <div style="display: flex; justify-content: space-between;">
+                                            <p class="text-base font-semibold text-white my-1">Harga: Rp<?= $row['hargamenu'] ?></p>
+                                            <button type="button" onclick="pesan(<?= $row['idmenu'] ?>)" class="flex gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-7 py-3 rounded-full border border-gray-600 hover:scale-105 duration-200 hover:text-gray-500 hover:border-gray-800 hover:from-black hover:to-gray-900">
+                                                Pesan
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endwhile; ?>
+                    </div>
+                </div>
+            </section>
+            <br>
+        <?php endforeach; ?>
     </div>
     <script>
         function login() {
@@ -156,6 +227,16 @@
         function register() {
             window.location.href = 'register.php';
 
+        }
+
+        function toggleDetails(idmenu) {
+            const detailsElement = document.getElementById(`details_${idmenu}`);
+            detailsElement.classList.toggle('menu-details-active');
+        }
+
+        function pesan(idmenu) {
+            // Lakukan aksi pesan di sini, misalnya memunculkan form pesan atau mengirimkan data ke server
+            console.log(`Pesan menu dengan ID ${idmenu}`);
         }
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
