da78236a6114c70b04bdb34e3a0bd774c8d839de
cowcow1 <stevanusfirman22@gmail.com>
2023-10-22 21:53:57 +0700

    update db.php

--- UTSLEC/db.php
+++ UTSLEC/db.php
@@ -2,7 +2,7 @@
 //DB Credentials
 define('DSN', 'mysql:host=localhost;dbname=utspemweblec');
 define('DBUSER', 'root');
-define('DBPASS', '');
+define('DBPASS', 'Stevanus22');
 
 //1. connect to DB
 $db = new PDO(DSN, DBUSER, DBPASS);
