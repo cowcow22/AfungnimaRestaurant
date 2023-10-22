d741dd99551b326232b4088c46b88e61f9a4a3a9
cowcow1 <stevanusfirman22@gmail.com>
2023-10-22 20:37:06 +0700

    Update DeleteMenu

--- UTSLEC/deleteMenu.php
+++ UTSLEC/deleteMenu.php
@@ -5,9 +5,15 @@ require_once('db.php');
 
 $idmenu = $_GET['idmenu'];
 
-$deleteSql = "DELETE FROM daftarmenu WHERE idmenu = :idmenu";
-$stmt = $db->prepare($deleteSql);
-$stmt->bindParam(':idmenu', $idmenu, PDO::PARAM_INT);
-$stmt->execute();
+$deleteOrderSql = "DELETE FROM `order` WHERE idmenu = :idmenu";
+$stmtOrder = $db->prepare($deleteOrderSql);
+$stmtOrder->bindParam(':idmenu', $idmenu, PDO::PARAM_INT);
+$stmtOrder->execute();
+
+$deleteMenuSql = "DELETE FROM daftarmenu WHERE idmenu = :idmenu";
+$stmtMenu = $db->prepare($deleteMenuSql);
+$stmtMenu->bindParam(':idmenu', $idmenu, PDO::PARAM_INT);
+$stmtMenu->execute();
+
 header('Location: adminPage.php');
 exit();
